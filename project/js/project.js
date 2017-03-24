cd6f2490f856effb6ee84c8f512ee4f01e80a7b2
dqbach <bach@techmasters-Mac-mini-5.local>
2017-03-24 17:48:26 +0700

    up

--- project/js/project.js
+++ project/js/project.js
@@ -2,4 +2,27 @@ $(document).ready(function(){
     $(".menu-button").click(function(){
         $(".main-menu-box").toggleClass("main-menu-box-click");
     });
+
+    $(".greater-than").click(function(){
+       $(".view-box").addClass("view-box-move-right-1");
+    });
+  
+
+// 	var slideIndex = 1;
+// 		showDivs(slideIndex);
+
+// 		function plusDivs(n) {
+// 		  showDivs(slideIndex += n);
+// 		}
+
+// 		function showDivs(n) {
+// 		  var i;
+// 		  var x = document.getElementsByClassName("product-box");
+// 		  if (n > x.length) {slideIndex = 1}    
+// 		  if (n < 1) {slideIndex = x.length}
+// 		  for (i = 0; i < x.length; i++) {
+// 		     x[i].style.display = "none";  
+// 		  }
+// 		  x[slideIndex-1].style.display = "block";  
+// 		}
 });
