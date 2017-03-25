cf91a01d9d1ecf16498f016e98e91cb3d7179e9b
dqbach <bach@techmasters-Mac-mini-5.local>
2017-03-25 17:46:41 +0700

    up

---
+++ project/js/detail-js.js
@@ -0,0 +1,1 @@
+var indexSlide=0;
--- project/js/project.js
+++ project/js/project.js
@@ -2,27 +2,14 @@ $(document).ready(function(){
     $(".menu-button").click(function(){
         $(".main-menu-box").toggleClass("main-menu-box-click");
     });
-
-    $(".greater-than").click(function(){
-       $(".view-box").addClass("view-box-move-right-1");
+    $(".contact-box-button").click(function(){
+        $(".contact-box").toggleClass("contact-box-up ");
     });
+    $(".contact-box-button").click(function(){
+        $(".contact-box-title").toggleClass("block");
+    });
+
   
 
-// 	var slideIndex = 1;
-// 		showDivs(slideIndex);
 
-// 		function plusDivs(n) {
-// 		  showDivs(slideIndex += n);
-// 		}
-
-// 		function showDivs(n) {
-// 		  var i;
-// 		  var x = document.getElementsByClassName("product-box");
-// 		  if (n > x.length) {slideIndex = 1}    
-// 		  if (n < 1) {slideIndex = x.length}
-// 		  for (i = 0; i < x.length; i++) {
-// 		     x[i].style.display = "none";  
-// 		  }
-// 		  x[slideIndex-1].style.display = "block";  
-// 		}
 });
