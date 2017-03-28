6d61acff10ea6c0e3100cc70fd1728402093360a
dqbach <bach@techmasters-Mac-mini-5.local>
2017-03-28 17:48:21 +0700

    up

--- project/js/login-form-js.js
+++ project/js/login-form-js.js
@@ -1,54 +1,73 @@
 
 
 
-function logInOnload() {
-	var outer = document.getElementById('outer');
-	var signIn = document.getElementById('sign-in-form');
-	var recover = document.getElementById('recover-form');
-	var signUp = document.getElementById('sign-up-form');
+// function logInOnload() {
+// 	var insideOfForm = document.getElementsByTagName('login-modal');
+// 	var signIn = document.getElementById('sign-in-form');
+// 	var recover = document.getElementById('recover-form');
+// 	var signUp = document.getElementById('sign-up-form');
 
-	var xBtn = document.querySelectorAll('#x');
-	var logInBtn = document.getElementById('login');
-	var dayBtn = document.getElementById('clickHere');
-	var taoTaiKhoanBtn = document.getElementById('createAccount');
+// 	var xBtn = document.querySelectorAll('#x');
+// 	var logInBtn = document.getElementById('login');
+// 	var dayBtn = document.getElementById('clickHere');
+// 	var taoTaiKhoanBtn = document.getElementById('createAccount');
 
-	// When the user clicks anywhere outside of the modal, close it
-	window.onclick = function(event) {
-		if (event.target == outer) {
-			signIn.style.display = "none";
-			recover.style.display = "none";
-			signUp.style.display = "none";
-		}
-		for (var i = 0; i < xBtn.length; i++) {
-			
-			if (event.target == xBtn[i]) {
-				signIn.style.display = "none";
-				recover.style.display = "none";
-				signUp.style.display = "none";
-			}
-		}
-		
+// 	// When the user clicks anywhere outside of the modal, close it
+// 	window.onclick = function(event) {
+// 		if (event.target == insideOfForm) {
+// 			signIn.style.display = "none";
+// 			recover.style.display = "none";
+// 			signUp.style.display = "none";
+// 		}
+// 		for (var i = 0; i < xBtn.length; i++) {
 
-		if (event.target == logInBtn) {
-			signIn.style.display = "block";
-			recover.style.display = "none";
-			signUp.style.display = "none";
-		}
+// 			if (event.target == xBtn[i]) {
+// 				signIn.style.display = "none";
+// 				recover.style.display = "none";
+// 				signUp.style.display = "none";
+// 			}
+// 		}
 
-		if (event.target == dayBtn) {
-			signIn.style.display = "none";
-			recover.style.display = "block";
-			signUp.style.display = "none";
-		}
 
-		if (event.target == taoTaiKhoanBtn) {
-			signIn.style.display = "none";
-			recover.style.display = "none";
-			signUp.style.display = "block";
-		}
-	};
-}
+// 		if (event.target == logInBtn) {
+// 			signIn.style.display = "block";
+// 			recover.style.display = "none";
+// 			signUp.style.display = "none";
+// 			// $(logInBtn).toggle();
+// 		}
 
-window.onload = function () {
-	logInOnload();
-};
+// 		if (event.target == dayBtn) {
+// 			signIn.style.display = "none";
+// 			recover.style.display = "block";
+// 			signUp.style.display = "none";
+// 		}
+
+// 		if (event.target == taoTaiKhoanBtn) {
+// 			signIn.style.display = "none";
+// 			recover.style.display = "none";
+// 			signUp.style.display = "block";
+// 		}
+// 	};
+// }
+
+// window.onload = function () {
+// 	logInOnload();
+// };
+
+$(document).ready(function () {
+	$('#show-recover').click(function (e) {
+		e.preventDefault();
+		$('#login-modal').modal('hide').on('hidden.bs.modal',function (e) {
+			$('#recover-modal').modal('show');
+			$(this).off('hidden.bs.modal');
+		});
+	});
+	$('#show-register').click(function (e) {
+		e.preventDefault();
+		$('#login-modal').modal('hide').on('hidden.bs.modal',function (e) {
+			$('#register-modal').modal('show');
+			$(this).off('hidden.bs.modal');
+		});
+	});
+
+});
--- project/js/project.js
+++ project/js/project.js
@@ -1,15 +1,26 @@
 $(document).ready(function(){
-    $(".menu-button").click(function(){
-        $(".main-menu-box").toggleClass("main-menu-box-click");
+
+	$('.carousel').carousel({
+	  interval: false
+	})
+
+    $('.carousel1').carousel({
+      interval: 3000
+    })
+
+
+	$(".menu-button").click(function(){
+        $(".main-menu-box").toggleClass("main-menu-box-move");
     });
+
     $(".contact-box-button").click(function(){
-        $(".contact-box").toggleClass("contact-box-up ");
+        $(".contact-box").toggleClass("contact-box-up");
     });
+    
     $(".contact-box-button").click(function(){
         $(".contact-box-title").toggleClass("block");
     });
 
-  
 
 
 });
